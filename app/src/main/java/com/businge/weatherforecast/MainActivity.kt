c5f333d695ec5600bf8e7b3071777458e12b4e32
Businge Brian <busingebrian@gmail.com>
2024-08-25 14:23:06 +0300

    permission to access lat and lon coordinates/ sec

--- app/src/main/java/com/businge/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/businge/weatherforecast/MainActivity.kt
@@ -1,24 +1,36 @@
 package com.businge.weatherforecast
 
+import android.annotation.SuppressLint
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.location.LocationManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
 import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 
 class MainActivity : AppCompatActivity() {
     private val REQUEST_LOCATION_CODE = 111
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        mFusedLocationClient =
+            LocationServices.getFusedLocationProviderClient(this) // get lat and long coordinates
 
         if (!isLocationEnabled()) {
             Toast.makeText(this@MainActivity, "The Location is not turned on", Toast.LENGTH_SHORT)
@@ -26,9 +38,38 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
             startActivity(intent)
         } else {
-          requestPermissions()
+            requestPermissions()
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_LOCATION_CODE && grantResults.isNotEmpty()) {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            requestLocationData()
+        } else {
+            Toast.makeText(this, "Permission Not Granted", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData() {
+        val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000).build()
+        mFusedLocationClient.requestLocationUpdates(locationRequest, object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "latitude : ${locationResult.lastLocation?.latitude} \n longitude : ${locationResult.lastLocation?.longitude}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }, Looper.myLooper())
+    }
+
 
     private fun isLocationEnabled(): Boolean {
         val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
